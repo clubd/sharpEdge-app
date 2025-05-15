afe5b15c8adf0afc4160670d1d0245421580ac1d
clubd <danenrmar@gmail.com>
2025-05-14 23:02:07 -0400

    create login syntax

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,16 +5,34 @@ import './Login.scss'
 
 function Login() {
     return (
-        <>
-            <header className="login__header">
-                <div className="login__top-box">
-                    <img src={sharpEdgeLogo} alt="Sharp Edge Logo" className="login__logo" />
-                </div>
-                <a href="/register" className="login__join">
-                    <Plus size={16} /> Join
-                </a>
-            </header>
-        </>
+        <div className="login__page">
+            <div className="login__container">
+                <header className="login__header">
+                    <div className="login__top-box">
+                        <img src={sharpEdgeLogo} alt="Sharp Edge Logo" className="login__logo" />
+                    </div>
+                    <a href="/register" className="login__join">
+                        <Plus size={16} /> Join
+                    </a>
+                </header>
+                <main>
+                    <form action="" className="login__form">
+                        <div className="login__group">
+                            <label htmlFor="email" className="visually__hidden">Email Address</label>
+                            <input className="login__input" type="email" id="email" name="email" placeholder="Email address" required />
+                        </div>
+                        <div className="login__group">
+                            <label htmlFor="password" className="visually__hidden">Password</label>
+                            <input className="login__input" type="password" id="password" name="password" placeholder="Password" />
+                        </div>
+                        <div className="reset__wrapper">
+                            <a href="#" className="login__reset">Forgot Password</a>
+                        </div>
+                        <button type="submit" className="login__btn">Login</button>
+                    </form>
+                </main>
+            </div>
+        </div>
     )
 }
 
